b46fca4b8a0d782b4d06818740406d2656e974df
Alexey Sosnoviy <int-it@yandex.ru>
2020-01-14 08:57:58 +0300

    env change

--- env.bat
+++ env.bat
@@ -1,3 +1,4 @@
+chcp 65001
 set DEBUGER_URL=http://localhost:1550
 set DEBUG_ADR=localhost
 set DEBUG_PORT=1550
@@ -6,7 +7,9 @@ set PROXY_RESULT_DIR=./log/project.log
 set PROXY_PORT=3000
 set PROXY_URL=http://localhost:3000
 set IBConnectionString=File=C:\DEVELOP\tmp\testForCover;
-set v8path="C:/Program Files/1cv8/8.3.14.1779/bin/"
+set v8path="C:/Program Files/1cv8/8.3.10.2753/bin/"
 
-set SRC_PATH=./fixtures/cf/
+set SRC_PATH=C:/DEVELOP/VA/va_cover
 set CONVERT_RESULT_DIR=./out/
+
+set USERNAME=Administrator
